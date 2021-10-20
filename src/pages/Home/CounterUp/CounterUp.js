d2bb3f43dd4183be6d140f77fcd7298062213db2
mariyapoly <poly.craft2141@gmail.com>
2021-10-20 16:42:31 +0600

    added counter component

--- src/pages/Home/CounterUp/CounterUp.js
+++ src/pages/Home/CounterUp/CounterUp.js
@@ -15,28 +15,28 @@ const CounterUp = () => {
                     <Col lg={3} xs={12} sm={6}>
                         <div className="single-counter">
                             <FontAwesomeIcon className="count-icon" icon={faTrophy} />
-                            <CountUp duration={3} end={201} />
+                            <CountUp delay={5} duration={3} end={201} />
                             <p>Award Shows</p>
                         </div>
                     </Col>
                     <Col lg={3} xs={12} sm={6}>
                         <div className="single-counter">
                             <FontAwesomeIcon className="count-icon" icon={faUsers} />
-                            <CountUp duration={3} end={354} />
+                            <CountUp delay={5} duration={3} end={354} />
                             <p>Qualified Staff</p>
                         </div>
                     </Col>
                     <Col lg={3} xs={12} sm={6}>
                         <div className="single-counter">
                             <FontAwesomeIcon className="count-icon" icon={faHospitalAlt} />
-                            <CountUp duration={3} end={528} />
+                            <CountUp delay={5} duration={3} end={528} />
                             <p>Hospital Rooms</p>
                         </div>
                     </Col>
                     <Col lg={3} xs={12} sm={6}>
                         <div className="single-counter">
                             <FontAwesomeIcon className="count-icon" icon={faHeartbeat} />
-                            <CountUp duration={3} end={124} />
+                            <CountUp delay={5} duration={3} end={124} />
                             <p>Machines</p>
                         </div>
                     </Col>
